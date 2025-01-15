157a09f555dfc34cea88b0969c64aa340e4996e2
massir-rpi <richiesmassi@gmail.com>
2025-01-14 22:41:25 -0500

    we can count triangles now :)

---
+++ count_triangles.py
@@ -0,0 +1,26 @@
+from poly_point_isect import isect_segments_include_segments
+
+def count_triangles(lines):
+    isects = isect_segments_include_segments(lines)
+    isects.sort(key=lambda x: x[0][0])
+    lasts = dict()
+    num_triangles = 0
+    for i, isect in isects:
+        if isect[0] in lasts and isect[1] in lasts and lasts[isect[0]] == lasts[isect[1]]:
+            num_triangles += 1
+            print("Found a triangle! @{} between lines {} and {}".format(i, isect[0], isect[1]))
+        lasts[isect[0]] = isect[1]
+        lasts[isect[1]] = isect[0]
+    return num_triangles
+
+if __name__ == '__main__':
+    # print(count_triangles([((-1,-1),(1,1)),((1,0),(-1,2)),((0,-1),(0,2))]))
+    print(count_triangles(
+        [
+            ((0,0),(8,0)),
+            ((0,-1),(6,5)),
+            ((0,7),(8,-1)),
+            ((0,3),(5,-2)),
+            ((3,-2),(8,3))
+        ]
+    ))
