65150286b6b336c8e7164c7cdfa676b80e40e6b9
massir-rpi <richiesmassi@gmail.com>
2025-01-14 23:54:23 -0500

    optimizer works now :) don't use gradient based optimizers on non-differentiable objective functions

--- optimize.py
+++ optimize.py
@@ -1,9 +1,9 @@
-from scipy.optimize import minimize
+from scipy.optimize import dual_annealing
 from count_triangles import count_triangle_flat_in_neg_out
 import numpy as np
 
 def optimize(lines):
-    return minimize(count_triangle_flat_in_neg_out, lines.flat, method='Nelder-Mead')
+    return dual_annealing(count_triangle_flat_in_neg_out, bounds=[[-(2**32)+1,2**32-1]]*lines.size, x0=lines.flatten())
 
 if __name__ == '__main__':
     lines = np.array([
@@ -11,7 +11,13 @@ if __name__ == '__main__':
         [[0., -1.], [6., 5.]],
         [[0., 7.], [8., -1.]],
         [[0., 3.], [5., -2.]],
-        [[3., -2.], [8., 3.]]
+        [[3., -2.], [8., 3.]],
+        #[[0., -4.], [8., 2.]],
+        #[[0., 8.], [8., -5.]],
+        #[[0., 6.], [8., 4.]],
+        #[[0., -3.], [8., 1.]],
+        #[[0., 1.], [8., -3.]],
+        #[[0., 2.], [8., -4.]],
     ])
     opt = optimize(lines)
     print("Found {} trianges with {} lines with lines:\n{}".format(int(-1 * opt.fun), len(lines), opt.x.reshape(opt.x.size // 4, 2, 2)))
