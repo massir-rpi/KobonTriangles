dbaa7a91478ee4f5dd1960a448ba1022a35de239
Matthew <matthew.bernstein@kitware.com>
2025-01-16 23:54:33 -0500

    view limits

--- view.py
+++ view.py
@@ -1,6 +1,18 @@
 import matplotlib.pyplot as plt
+from typing import Optional, Tuple
 
-def plot_lines_in_circle(line_segments, zoom_factor, filepath):
+def plot_lines_in_circle(line_segments, zoom_factor, filepath, limits: Optional[Tuple[Tuple[float]]] = None):
+    """
+    Args:
+        limits:
+            nested tuples ((xmin, xmax), (ymin, ymax))
+            if not provided, zoom_factor will be used
+    """
+
+    if limits is None:
+        limit = 1.1 * zoom_factor
+        limits = ((limit, limit), (limit, limit))
+
     N = len(line_segments)
     fig, ax = plt.subplots()
 
@@ -16,8 +28,8 @@ def plot_lines_in_circle(line_segments, zoom_factor, filepath):
         x2, y2 = line_segments[i][1]
         ax.plot([x1, x2], [y1, y2], marker='o')
 
-    ax.set_xlim(-1.1 * zoom_factor, 1.1 * zoom_factor)
-    ax.set_ylim(-1.1 * zoom_factor, 1.1 * zoom_factor)
+    ax.set_xlim(**limits[0])
+    ax.set_ylim(**limits[1])
     ax.set_aspect('equal', adjustable='box')
 
     plt.savefig(filepath)
