7f5090251354ff53ed599ed3c6e1e3f819cc3889
Matthew <matthew.bernstein@kitware.com>
2025-01-16 23:41:02 -0500

    make the line segments at circle

--- generate_line_segments.py
+++ generate_line_segments.py
@@ -20,9 +20,14 @@ def generate_point_pairs_on_circle(angles):
     # zoom out
     slopes = (arr[:, 1, 1] - arr[:, 1, 0]) / (arr[:, 0, 1] - arr[:, 0, 0])
     intercepts = arr[:, 1, 0] - slopes * arr[:, 0, 0]
-    new_x1 = -ZOOM_FACTOR * np.ones(N_POINTS)
+
+    # truncate at edges of big circle - solve x^2+y^2=r^2 && y=mx+b
+    m = slopes
+    b = intercepts
+    r = ZOOM_FACTOR
+    new_x1 = - (np.sqrt((m ** 2 + 1) * r ** 2 - b ** 2) + b * m) / (m ** 2 + 1)
+    new_x2 =   (np.sqrt((m ** 2 + 1) * r ** 2 - b ** 2) - b * m) / (m ** 2 + 1)
     new_y1 = slopes * new_x1 + intercepts
-    new_x2 = ZOOM_FACTOR * np.ones(N_POINTS)
     new_y2 = slopes * new_x2 + intercepts
 
     # back to point-point form
